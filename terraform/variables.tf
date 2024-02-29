abc7f05b1276b0f1397d8e9537dabcc4db09b37a
avinashcherukuri9 <avinash.cherukuri.9@gmail.com>
2024-02-29 17:20:03 +0530

    adding terraform files which modifed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,7 +27,7 @@ variable "private_subnet_2_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-west-1b", "us-west-1c"]
+  default     = ["us-west-1a", "us-west-1c"]
 }
 
 
@@ -67,7 +67,7 @@ variable "log_retention_in_days" {
 
 variable "ssh_pubkey_file" {
   description = "Path to an SSH public key"
-  default     = "/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainers/terraform/california-region-key-pair.pub"
+  default     = "C:/Users/Avinash/Desktop/Praveen_Devops/Terraform_withcontainers/terraform/california-region-key-pair.pub"
 }
 
 
@@ -89,7 +89,7 @@ variable "instance_type" {
 }
 variable "docker_image_url_django" {
   description = "Docker image to run in the ECS cluster"
-  default     = "600735812827.dkr.ecr.us-west-1.amazonaws.com/django-app:latest"
+  default     = "992382500495.dkr.ecr.us-west-1.amazonaws.com/django-app:latest"
 }
 variable "app_count" {
   description = "Number of Docker containers to run"
